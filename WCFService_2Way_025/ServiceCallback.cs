2ec947706bf109fd743b991adf45e2087e61699e
Riyantama <62780613+ryntm23@users.noreply.github.com>
2021-01-15 08:59:27 +0700

    Add project files.

---
+++ WCFService_2Way_025/ServiceCallback.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_025
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceCallback : IServiceCallback
+    {
+        Dictionary<IClientCallback, string> userlist = new Dictionary<IClientCallback, string>();
+
+        public void gabung(string username)
+        {
+            IClientCallback koneksiGabung = OperationContext.Current.GetCallbackChannel<IClientCallback>();
+            userlist[koneksiGabung] = username;
+        }
+
+        public void kirimPesan(string pesan)
+        {
+            IClientCallback koneksiPesan = OperationContext.Current.GetCallbackChannel<IClientCallback>();
+            string user;
+            if (!userlist.TryGetValue(koneksiPesan, out user))
+            {
+                return;
+            }
+            foreach (IClientCallback other in userlist.Keys)
+            {
+                other.pesanKirim(user, pesan);
+            }
+        }
+    }
+}
